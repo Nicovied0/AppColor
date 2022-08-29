0f4584c085fed59e0b988ea6a099fae01c3eff01
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-08-29 02:04:15 -0300

    function double click

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let colorPiker = document.getElementById('colorPiker')
 
-colorPiker.addEventListener('input', e =>{
+colorPiker.addEventListener('input', e => {
   let background = document.getElementById('body')
   background.style.background = e.target.value
 })
@@ -10,19 +10,24 @@ colorPiker.addEventListener('input', e =>{
 function toastFunction() {
   var x = document.getElementById("toast");
   x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 
 window.addEventListener('load', init, false);
-        function init() {
-            let div = document.querySelector('.container');
-            div.style.visibility = 'visible';
-            let boton = document.querySelector('#btn');
-            boton.addEventListener('click', function (e) {
-                if(div.style.visibility === 'visible'){
-                    div.style.visibility = 'hidden';
-                }else{
-                    div.style.visibility = 'visible';
-                }
-            }, false);
-        }
+function init() {
+  let div = document.querySelector('.container');
+  div.style.visibility = 'visible';
+  let boton = document.querySelector('#btn');
+  boton.addEventListener('click', function (e) {
+    if (div.style.visibility === 'visible') {
+      div.style.visibility = 'hidden';
+    }
+  }, false);
+  
+};
+
+
+
+function myFunction() {
+  document.getElementById("body") = "Hello World";
+}
